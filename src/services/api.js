2d82a25f76e0d544a6d65a95ab5a5222c80a8c25
Roza-mekete <bitaniadereje05@gmail.com>
2025-06-01 13:35:14 +0400

    api

---
+++ src/services/api.js
@@ -0,0 +1,25 @@
+// src/services/api.js
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'https://driving-backend-stmb.onrender.com/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Add request interceptor to include auth token
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
